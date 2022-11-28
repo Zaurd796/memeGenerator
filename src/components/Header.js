300ea8c7c1342a270a3fdf7f3f0a48cf9bce3d4e
Zaur <zaurdysh@gmail.com>
2022-11-28 06:05:33 +0300

    first commit

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import trollFase from "../images/troll-face.png"
+
+export default function Header() {
+    return (
+        <header className="header">
+            <img
+                src={trollFase}
+                className="header--image"
+                alt="troll fase"
+            />
+            <h2 className="header--title">Meme Generator</h2>
+            <h4 className="header--project">React Course - Project 3</h4>
+        </header>
+    )
+}
